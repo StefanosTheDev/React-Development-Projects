225cc28c25841ae5ab965a9f214d7795402462fd
StefanosTheDev <stefanosdev26@gmail.com>
2025-02-01 19:39:57 -0800

    push

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,32 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import { Crud } from './crud';
 function App() {
-  return <div> Hello </div>;
-}
+  const users = ['John Doe', 'Jane Smith', 'Alice Johnson', 'Bob Brown'];
 
-// CRUD
-
-function Tasks({ tasks }) {
-  // This is going to return the list
-  return <div className="tasks"></div>;
-}
-
-// Parent componenet (App) passes userName as a prop to the Child Componeent (Greeting usesing this line. )
-// NAME is the Prop Key . The name of the property thats passed to the child
-// USERNAME is the variable holding the value being passed.
-
-// Child Componenet
-function Greeting({ name, age }) {
   return (
-    <h1>
-      {' '}
-      Hello, {name} {age}{' '}
-    </h1>
+    <div className="app">
+      <h1> From APP </h1>
+    </div>
   );
 }
-
-function CustomButton({ onClick }) {
-  return <button onClick={onClick}> CLick Me </button>;
-}
-
 export default App;
+
+// Okay so lets load this.
